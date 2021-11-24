647c90cd40308aa460fa2ed74dd579baedfe707c
Mark Hendriks <mark.hendriks@hybrit.org>
2021-11-24 18:57:09 +0100

    Fix layout, disable buttons while processing files

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::env;
 use std::fmt::Display;
 use std::fs::canonicalize;
 use std::path::PathBuf;
-
+use iced::*;
 use iced::{
     button, executor, Align, Application, Button, Clipboard, Column, Command, Element, Length, Row, Rule,
     Settings, Subscription, Text,
@@ -23,7 +23,7 @@ fn main() {
 
 fn open_ui() -> iced::Result {
     let mut settings = Settings::default();
-    settings.window.size = (800, 600);
+    settings.window.size = (400, 300);
     MotionSplit::run(settings)
 }
 
@@ -57,6 +57,7 @@ struct MotionSplit {
     pick_file_button: button::State,
     pick_directory_button: button::State,
     convert_button: button::State,
+    path_display: text_input::State,
 }
 
 #[derive(Debug, Clone)]
@@ -65,6 +66,7 @@ pub enum Message {
     SelectDirectory,
     Convert,
     ExtractUpdate(extract_task::Update),
+    Noop,
 }
 
 impl Application for MotionSplit {
@@ -173,45 +175,58 @@ impl Application for MotionSplit {
         if cfg!(windows) {
             path_message = path_message.trim_start_matches(r"\\?\");
         }
+
+        let mut pick_file = Button::new(&mut self.pick_file_button, Text::new("Select file"));
+        let mut pick_directory = Button::new(
+                                    &mut self.pick_directory_button,
+                                    Text::new("Select directory"),
+                                );
+        let mut convert = Button::new(&mut self.convert_button, Text::new("Convert file(s)"));
+
+        if !self.converting {
+            pick_file = pick_file.on_press(Message::SelectFile);
+            pick_directory = pick_directory.on_press(Message::SelectDirectory);
+            convert = convert.on_press(Message::Convert);
+        }
+        self.path_display.unfocus();
+
         Column::new()
             .push(
                 Column::new()
                     .width(iced::Length::Fill)
                     .height(iced::Length::Fill)
                     .padding(20)
+                    .spacing(5)
                     .align_items(Align::Center)
+                    .push(
+                        TextInput::new(&mut self.path_display, path_message, path_message, |_| Message::Noop)
+                            .padding(3)
+                    )
                     .push(
                         Row::new()
                             .spacing(20)
                             .align_items(Align::Center)
-                            .push(
-                                Button::new(&mut self.pick_file_button, Text::new("Select file"))
-                                    .on_press(Message::SelectFile),
-                            )
-                            .push(
-                                Button::new(
-                                    &mut self.pick_directory_button,
-                                    Text::new("Select directory"),
-                                )
-                                .on_press(Message::SelectDirectory),
-                            ),
-                    )
-                    .push(Text::new(path_message).size(20))
-                    .push(
-                        Button::new(&mut self.convert_button, Text::new("Convert file(s)"))
-                            .on_press(Message::Convert),
+                            .push(pick_file)
+                            .push(pick_directory)
+                            .push(Space::new(Length::Fill, Length::Shrink))
                     )
             )
             .push(
                 Column::new()
                     .align_items(Align::Center)
+                    .push(Row::new()
+                        .padding(10)
+                        .align_items(Align::End)
+                        .push(Space::new(Length::Fill, Length::Shrink))
+                        .push(convert)
+                    )
                     .push(Rule::horizontal(0))
                     .push(Row::new()
                         .padding(10)
                         .align_items(Align::Start)
                         .push(Text::new("Status: "))
                         .push(Text::new(
-                            Some("Hello World")
+                            self.status
                                 .as_ref()
                                 .map(|s| s.to_string())
                                 .unwrap_or_else(|| "".to_string())
